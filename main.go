02aef3cf69110daa64eb5f639f3bc91d1e39651d
Kai Hendry <hendry@iki.fi>
2023-07-24 18:39:35 +0100

    set slog json logger as default

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/kaihendry/firstcobra/cmd"
+import (
+	"os"
+
+	"github.com/kaihendry/firstcobra/cmd"
+	"golang.org/x/exp/slog"
+)
 
 func main() {
+	slog.SetDefault(slog.New(slog.NewJSONHandler(os.Stderr, nil)))
 	cmd.Execute()
 }
